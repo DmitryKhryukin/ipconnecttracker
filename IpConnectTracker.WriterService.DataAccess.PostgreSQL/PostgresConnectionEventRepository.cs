c30120f60fd5a70351bb0645a1a7b287b9c2abf9
Dmitry Khryukin <dmitry.khryukin@gmail.com>
2025-05-01 17:35:32 +0300

    added implementation to PostresConnectionEventRepository

--- IpConnectTracker.WriterService.DataAccess.PostgreSQL/PostgresConnectionEventRepository.cs
+++ IpConnectTracker.WriterService.DataAccess.PostgreSQL/PostgresConnectionEventRepository.cs
@@ -1,6 +1,8 @@
+using Dapper;
 using IpConnectTracker.WriterService.DataAccess.Abstractions;
 using IpConnectTracker.WriterService.DataAccess.PostgreSQL.Config;
 using Microsoft.Extensions.Options;
+using Npgsql;
 
 namespace IpConnectTracker.WriterService.DataAccess.PostgreSQL;
 
@@ -15,6 +17,15 @@ public class PostgresConnectionEventRepository : IConnectionEventRepository
 
     public async Task StoreAsync(long userId, string ipAddress, DateTime timestamp, CancellationToken cancellationToken)
     {
-       //TODO: write to postgres
+        const string sql = "INSERT INTO user_connection_events (user_id, ip, timestamp) VALUES (@UserId, @Ip, @Timestamp);";
+
+        await using var connection = new NpgsqlConnection(_connectionString);
+        await connection.ExecuteAsync(sql, 
+            new
+            {
+                UserId = userId, 
+                Ip = ipAddress, 
+                Timestamp = timestamp
+            });
     }
 }
