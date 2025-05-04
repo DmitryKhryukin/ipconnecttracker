280de68a07eceec41131c336e61f464b78423d49
Dmitry Khryukin <dmitry.khryukin@gmail.com>
2025-05-04 09:54:37 +0300

    test update

--- src/IpConnectTracker.WriterService.UnitTests/Services/MessageProcessorServiceTests.cs
+++ src/IpConnectTracker.WriterService.UnitTests/Services/MessageProcessorServiceTests.cs
@@ -82,16 +82,23 @@ public class MessageProcessorServiceTests
         var backgroundTask = service.StartAsync(cancellationToken.Token);
 
         long userId = 1001;
-        var validIpAddress = "127.0.0.1";
-        var validMessage = $"{userId},{validIpAddress}";
+        var validIpAddress1 = "127.0.0.1";
+        var validIpAddress2 = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";
+        var validIpAddress3 = "ff06::c3";
+        var validIpAddress4 = "::ffff:192.1.56.10";
+        
+        var validMessage1 = $"{userId},{validIpAddress1}";
+        var validMessage2 = $"{userId},{validIpAddress2}";
+        var validMessage3 = $"{userId},{validIpAddress3}";
+        var validMessage4 = $"{userId},{validIpAddress4}";
         
         var invalidIpAddress = "invalid_address";
         var invalidMessage = $"{userId},{invalidIpAddress}";
         
-        await service.EnqueueAsync(validMessage); 
-        await service.EnqueueAsync(validMessage); 
-        await service.EnqueueAsync(validMessage); 
-        await service.EnqueueAsync(validMessage);
+        await service.EnqueueAsync(validMessage1); 
+        await service.EnqueueAsync(validMessage2); 
+        await service.EnqueueAsync(validMessage3); 
+        await service.EnqueueAsync(validMessage4);
         
         await service.EnqueueAsync(invalidMessage); 
         await service.EnqueueAsync(invalidMessage); 
